076d0aa5c0f630def81b0fdc3b2b6ca360f96600
nikodem <nikodemmarek11@gmail.com>
2024-04-21 19:45:18 +0000

    feat: nix setup

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{pkgs ? import <nixpkgs> {}}: let
+  alias-run = pkgs.writeShellScriptBin "r" ''cargo -Z unstable-options -C server run'';
+  alias-dev = pkgs.writeShellScriptBin "d" ''cargo-watch -C server -x run -c'';
+  alias-test = pkgs.writeShellScriptBin "t" ''cargo-watch -C server -x test -c'';
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs;
+      [
+        gcc
+        cargo
+        cargo-watch
+      ]
+      ++ [alias-run alias-dev alias-test];
+    shellHook = ''
+      printf "\e[33m
+        \e[1mr\e[0m\e[33m  -> run
+        \e[1md\e[0m\e[33m  -> dev
+        \e[1mt\e[0m\e[33m -> tests
+      \e[0m"
+    '';
+  }
