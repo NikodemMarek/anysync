bf8c6330be23f0feba8313085657f4952c4e7f96
nikodem <nikodemmarek11@gmail.com>
2024-04-30 09:31:25 +0000

    feat: cli params

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,13 +2,12 @@ use eyre::{Ok, Result};
 use std::{fs, net::SocketAddr, path::PathBuf};
 use axum::{extract::{Json, Path}, http::StatusCode, routing::{get, Router}};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tower_http::{services::ServeDir, trace::{DefaultMakeSpan, TraceLayer}};
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+use clap::Parser;
 
 mod ws;
 
 use ws::ws_handler;
-
-const ROOT_PATH: &str = "../testdir";
 
 fn get_files(path: &PathBuf) -> Result<Vec<PathBuf>> {
     let files = fs::read_dir(path)?
@@ -27,22 +26,31 @@ fn get_files(path: &PathBuf) -> Result<Vec<PathBuf>> {
     Ok(files)
 }
 
-async fn get_abs_paths() -> (StatusCode, Json<Vec<String>>) {
-    let root_path = PathBuf::from(ROOT_PATH);
+async fn get_abs_paths(root_path: PathBuf) -> (StatusCode, Json<Vec<String>>) {
+    let files = get_files(&root_path).unwrap();
+    (StatusCode::OK, Json(files.iter().map(|p| p.strip_prefix(&root_path).unwrap().to_str().unwrap().to_string()).collect()))
+}
+async fn get_paths(root_path: PathBuf, Path(path): Path<String>) -> (StatusCode, Json<Vec<String>>) {
+    let root_path = root_path.join(path.as_str());
 
     let files = get_files(&root_path).unwrap();
     (StatusCode::OK, Json(files.iter().map(|p| p.strip_prefix(&root_path).unwrap().to_str().unwrap().to_string()).collect()))
 }
-async fn get_paths(Path(path): Path<String>) -> (StatusCode, Json<Vec<String>>) {
-    let root_path = PathBuf::from(ROOT_PATH).join(path.as_str());
 
-    let files = get_files(&root_path).unwrap();
-    (StatusCode::OK, Json(files.iter().map(|p| p.strip_prefix(&root_path).unwrap().to_str().unwrap().to_string()).collect()))
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "../testdir")]
+    root_path: PathBuf,
+
+    #[arg(short, long, default_value = "5060")]
+    port: u16,
 }
 
 #[tokio::main]
 async fn main() {
-    let root_path = PathBuf::from(ROOT_PATH);
+    let Args { root_path, port } = Args::parse();
+
     let files = get_files(&root_path).unwrap();
 
     tracing_subscriber::registry()
@@ -54,16 +62,21 @@ async fn main() {
         .init();
 
     let app = Router::new()
-        .route("/paths", get(get_abs_paths))
-        .route("/paths/:path", get(get_paths))
-        .nest_service("/data", ServeDir::new(&root_path.clone()))
+        .route("/paths", get({
+            let root_path = root_path.clone();
+            move || get_abs_paths(root_path.clone())
+        }))
+        .route("/paths/:path", get({
+            let root_path = root_path.clone();
+            move |path| get_paths(root_path.clone(), path)
+        }))
         .route("/:file", get(ws_handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
         );
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:5060").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
     axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
 
     dbg!(files);
